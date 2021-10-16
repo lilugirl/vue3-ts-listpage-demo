1dfb893fe37cb8552fce708e5c342445efd91ba5
liu yi <lilugirl2005@126.com>
2021-10-16 20:08:03 +0800

    使用PropType定义props类型

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,58 @@
 <template>
-  <div>
-    app
+  <div class="app">
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+  import { defineComponent, reactive, ref, toRefs } from "vue";
+  import JobList from "./components/JobsList.vue";
+  import Job from "./types/Job";
 
   export default defineComponent({
     name: "App",
-    components: {},
+    components: { JobList },
+    setup() {
+      const jobs = ref<Job[]>([
+        {
+          title: "farm worker",
+          location: "lon lon ranch",
+          salary: 30000,
+          id: "1",
+        },
+        {
+          title: "quarryman",
+          location: "death mountain",
+          salary: 40000,
+          id: "2",
+        },
+        {
+          title: "flute player",
+          location: "the lost woods",
+          salary: 35000,
+          id: "3",
+        },
+        {
+          title: "fisherman",
+          location: "lake hylia",
+          salary: 21000,
+          id: "4",
+        },
+        {
+          title: "perison guard",
+          location: "gerudo valley",
+          salary: 32000,
+          id: "5",
+        },
+      ]);
+      return {
+        jobs,
+      };
+    },
+
+    methods: {},
   });
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} repees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
+            ut dolores veritatis. Magni saepe molestiae quae provident rerum
+            velit non, laudantium dolorem. Voluptatum beatae odio placeat! Error
+            explicabo pariatur fugit.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+  import Job from "@/types/Job";
+  import { defineComponent, PropType } from "vue";
+
+  export default defineComponent({
+    props: {
+      jobs: {
+        required: true,
+        type: Array as PropType<Job[]>,
+      },
+    },
+  });
+</script>
+<style scoped>
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+</style>
